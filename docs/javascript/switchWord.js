005a48d154bd62898336363bf63ee17bce0a04b5
Toby Bessant <toby.bessant.1998@gmail.com>
2020-01-03 18:36:34 +0000

    Added index content and style

---
+++ docs/javascript/switchWord.js
@@ -0,0 +1,17 @@
+const words = [ "communication", "maintenance" ];
+
+let currentWordIndex = 0;
+
+function switchText() {
+    const elementToChange = document.getElementById("word-changer");
+
+    let nextIndex = (++currentWordIndex) % words.length
+    let word = words[nextIndex];
+
+    elementToChange.classList.toggle("fade-out");
+
+    setTimeout(() => {
+        elementToChange.classList.toggle("fade-out");
+        elementToChange.innerHTML = word;
+    }, 1000);
+}
